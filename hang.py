0188c6c962e26b8a70879d0ef3a78476026cc7e4
gabrielademoraes <gabrielademoraes10@gmail.com>
2018-04-05 14:24:28 -0300

    Modulating loadWords function.

--- hang.py
+++ hang.py
@@ -1,23 +1,36 @@
+#! /usr/bin/python
 import random
 import string
+import time
+from os import system
 
 WORDLIST_FILENAME = "palavras.txt"
 
-def loadWords():
+def openArchive():
+    inFile = open(WORDLIST_FILENAME, 'r', 0)
+    return inFile
+
+def readArchive(inFile):
+    line = inFile.readline()
+    return line
+
+def makeListOfWords(line):
+    wordlist = string.split(line)
+    return wordlist
+
+def chooseWord(wordlist):
+    return random.choice(wordlist)
+
+def printLoadSituation(wordlist):
     """
     Depending on the size of the word list, this function may
     take a while to finish.
     """
     print "Loading word list from file..."
-    # inFile: file
-    inFile = open(WORDLIST_FILENAME, 'r', 0)
-    # line: string
-    line = inFile.readline()
-    # wordlist: list of strings
-    wordlist = string.split(line)
-    print "  ", len(wordlist), "words loaded."
-    return random.choice(wordlist)
-
+    time.sleep(2)
+    print len(wordlist), "words loaded."
+    print "Press ENTER to start"
+    raw_input()
 
 def isWordGuessed(secretWord, lettersGuessed):
     secretLetters = []
@@ -47,18 +60,16 @@ def getAvailableLetters():
     import string
     # 'abcdefghijklmnopqrstuvwxyz'
     available = string.ascii_lowercase
-
-
     return available
 
 def hangman(secretWord):
-
     guesses = 8
     lettersGuessed = []
+    system("clear")
     print 'Welcome to the game, Hangam!'
     print 'I am thinking of a word that is', len(secretWord), ' letters long.'
     print '-------------'
-
+    time.sleep(2)
     while  isWordGuessed(secretWord, lettersGuessed) == False and guesses >0:
         print 'You have ', guesses, 'guesses left.'
 
@@ -103,15 +114,18 @@ def hangman(secretWord):
 
             print 'Oops! That letter is not in my word: ',  guessed
         print '------------'
-
     else:
         if isWordGuessed(secretWord, lettersGuessed) == True:
             print 'Congratulations, you won!'
         else:
-            print 'Sorry, you ran out of guesses. The word was ', secretWord, '.'
+            print 'Sorry, you ran out of guesses. The word was \033[45m', secretWord, '\033[0m .'
 
-
-
-
-secretWord = loadWords().lower()
+# inFile: file
+inFile = openArchive()
+# line: string
+line = readArchive(inFile)
+# wordlist: list of strings
+wordlist = makeListOfWords(line)
+printLoadSituation(wordlist)
+secretWord = chooseWord(wordlist).lower()
 hangman(secretWord)
